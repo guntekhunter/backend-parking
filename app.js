a43590b84b7705ca8eb5a2174c5e52b2b8aefd3a
guntekhunter <guntek2000@gmail.com>
2023-09-18 19:38:13 +0800

    fix: again

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const { v4 } = require("uuid");
-
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
 
 const app = express();
 
